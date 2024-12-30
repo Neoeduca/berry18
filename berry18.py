bc456edb84fbc6e17c6b9cf4051dfe6c6b8d21e5
Neoeduca <informacion.neoeduca@gmail.com>
2024-12-30 15:50:36 -0300

    Update berry18.py

--- berry18.py
+++ berry18.py
@@ -87,25 +87,19 @@ class Auto:
             self.motor2._girar(self.motor1.grados, self.motor1.grados)
             self.estado=3
 
-SOUND_SPEED=340
-TRIG_PULSE_DURATION_US=10
-class HCSR04:
 
-        
+class HCSR04:   
     def __init__(self):
         self.trig_pin = Pin(18, Pin.OUT) 
         self.echo_pin = Pin(19, Pin.IN)
-
+        self.SOUND_SPEED=340
     def medir(self):
-        # Prepare le signal
         self.trig_pin.value(0)
         sleep_us(5)
-        # Créer une impulsion de 10 µs
         self.trig_pin.value(1)
-        sleep_us(TRIG_PULSE_DURATION_US)
+        sleep_us(10)
         self.trig_pin.value(0)
-
         ultrason_duration = time_pulse_us(self.echo_pin, 1, 30000) # Renvoie le temps de propagation de l'onde (en µs)
-        distance_cm = SOUND_SPEED * ultrason_duration / 20000
+        distance_cm = self.SOUND_SPEED * ultrason_duration / 20000
         return distance_cm
 
