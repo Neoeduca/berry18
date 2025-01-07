a99ac0cf5d58714830a6195d54d417298f022c08
Neoeduca <informacion.neoeduca@gmail.com>
2025-01-07 17:56:29 -0300

    Update motores.py

--- motores.py
+++ motores.py
@@ -1,85 +1,82 @@
-from time import sleep, sleep_us
-from machine import Pin, PWM, time_pulse_us
+import utime as time
+from machine import Pin, PWM
 
-class Servo360:
+class servo360:
     def __init__(self, servo_pin:int):
         self.servo = PWM(Pin(servo_pin))
         self.servo.freq(50)
         self.grados_anterior= 90
         
     def girar(self, grados):
-        if(self.grados_anterior != grados):
-            self.grados_anterior = grados
-            self._girar(grados, grados)
-            
-    
-    def detener(self):
-        self.girar(90)
-    
-    def _girar(self, grados1, grados2):
-        duty = int((12.346*grados1**2 + 7777.8*grados2 + 700000))
+        duty = int((12.346*grados**2 + 7777.8*grados + 700000))
         self.servo.duty_ns(duty)
-
-
+        
+class carro:
+    def __init__(self, motor_pin1, motor_pin2):
+        
+        _servo_pin1 = Pin(motor_pin1 )
+        _servo_pin2 = Pin(motor_pin2 )
+        self._servo_1 = PWM(_servo_pin1)
+        self._servo_2 = PWM(_servo_pin2)
+        self._servo_1.freq(50)
+        self._servo_2.freq(50)
+        self._grados_1=90
+        self._grados_2=90
+        self._grados_fast1=90
+        self._estado=0
+        self._grados_fast2=90
+        
+    def calibrar(self,grados_1, grados_2):
+        self._girar(grados_1, grados_2)
+        time.sleep_ms(5000)
+        print("frenando....")
+        self.movedetener()
+        print("Si no frena, requiere calibración para detener")
+        time.sleep_ms(1000)  
+        
+        
+    def setvelocidad1(self, grados_1, grados_2):
+        self._grados_1=grados_1
+        self._grados_2=grados_2
     
+    def setvelocidad2(self, grados_1, grados_2):
+        self._grados_fast1=grados_1
+        self._grados_fast2=grados_2
+        
+        
+    def moveadelanteSpeed1(self):
+        if self._estado!=1:
+            self._girar(self._grados_1,self._grados_2)
+            self._estado=1
 
-class Auto:
-    def __init__(self, pin_motor1, pin_motor2):
-        self.motor1 = Servo360(pin_motor1)
-        self.angulo_motor1 = 90
-        self.angulo2_motor1 = 90
-        
-        self.motor2 = Servo360(pin_motor2)
-        self.angulo_motor2 = 90
-        self.angulo2_motor2 = 90
-        
-        self.estado = 0
-        
-    def probar(self, grados_1, grados_2):
-        print("probando con ", grados_1, " y ", grados_2)
-        self.motor1.girar(grados_1)
-        self.motor2.girar(grados_2)
-        sleep(3)  
-        print("frenando con 90")
-        self.motor1.detener()
-        self.motor2.detener()
-        print("Si no frena, requiere calibración manual")
-        sleep(1)  
-    
-    def angulos(self, grados_1, grados_2):
-        self.angulo_motor1 = grados_1
-        self.angulo_motor2 = grados_2
-    def angulos2(self, grados_1, grados_2):
-        self.angulo2_motor1 = grados_1
-        self.angulo2_motor2 = grados_2
-        
-    def avanzar(self):
-        if self.estado!=1:
-            self.motor1.girar(self.angulo_motor1)
-            self.motor2.girar(self.angulo_motor2)
-            self.estado=1
-        
-    def detener(self):
-        if self.estado!=4:
-            self.motor1.detener()
-            self.motor2.detener()
-            self.estado=4
-
-    def retroceder(self):
-        if self.estado!=6:
-            self.motor1._girar(self.angulo_motor1, self.angulo_motor2)
-            self.motor2._girar(self.angulo_motor2, self.angulo_motor1)
-            self.estado=6
+    def moveadelanteSpeed2(self):
+        if self._estado!=5:
+            self._girar(self._grados_fast1,self._grados_fast2)
+            self._estado=5
             
-    def izquierda(self):
-        if self.estado!=2:
-            self.motor1.girar(self.angulo_motor2)
-            self.motor2.girar(self.angulo_motor2)
-            self.estado=2
+    def movedetener(self):
+        if self._estado!=4:
+            self._servo_1.duty_ns(0)
+            self._servo_2.duty_ns(0) 
+            self._estado=4
 
-    def derecha(self):
-        if self.estado!=3:
-            self.motor1.girar(self.angulo_motor1)
-            self.motor2.girar(self.angulo_motor1)
-            self.estado=3
+    def moveatras(self):
+        if self._estado!=6:
+            self._girar(self._grados_2,self._grados_1)
+            self._estado=6
             
+    def moveizquierda(self):
+        if self._estado!=2:
+            self._girar(self._grados_2,self._grados_2)
+            self._estado=2
+
+    def movederecha(self):
+        if self._estado!=3:
+            self._girar(self._grados_1,self._grados_1)
+            self._estado=3
+
+    def _girar(self, grado1, grado2):
+            duty1 = int((12.346*grado1**2 + 7777.8*grado1 + 700000))
+            duty2 = int((12.346*grado2**2 + 7777.8*grado2 + 700000))
+            self._servo_1.duty_ns(duty1)
+            self._servo_2.duty_ns(duty2) 
